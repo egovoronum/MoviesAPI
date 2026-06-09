f51714db1524490851a186186b82856afff7c53c
Egor <egor.voronkov@gmail.com>
2026-06-09 09:17:12 +0300

    - added user_api.py to git

---
+++ clients/user_api.py
@@ -0,0 +1,22 @@
+from custom_requester.custom_requester import CustomRequester
+
+class UserAPI(CustomRequester):
+    def __init__(self, session):
+        super().__init__(base_url=session.base_url)
+        self.session = session
+        
+    def get_user_info(self, user_id, expected_status=200):
+        return self.send_request(
+            method="GET",
+            endpoint=f"/users/{user_id}",
+            expected_status=expected_status
+        )
+        
+    def delete_user(self, user_id, expected_status=204):
+        return self.send_request(
+            method="DELETE",
+            endpoint=f"/users/{user_id},
+            expected_status=expected_status
+        )
+        
+        
