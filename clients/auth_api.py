66bfe9f09309e69c1bbac1ab9cdada15eb79554a
Egor <egor.voronkov@gmail.com>
2026-06-09 08:32:13 +0300

    - provisioned base func for AuthAPI class for user authentification

--- clients/auth_api.py
+++ clients/auth_api.py
@@ -1,9 +1,26 @@
-from constants import REGISTER_ENDPOINT
+from constants import REGISTER_ENDPOINT, LOGIN_ENDPOINT
 from custom_requester.custom_requester import CustomRequester
 
 class AuthAPI(CustomRequester):
+    
     def __init__(self, session):
         super().__init__(session=session, base_url="https://auth.dev-cinescope.coconutqa.ru/")
         
-
     
+    def register_user(self, user_data, expected_status=201):
+        return self.send_request(
+            method="POST",
+            endpoint=REGISTER_ENDPOINT,
+            data=user_data,
+            expected_status=expected_status
+        )
+        
+    def login_user(self, login_data, expected_status=201):
+        return self.send_request(
+            method="POST",
+            endpoint=LOGIN_ENDPOINT,
+            data=login_data,
+            expected_status=expected_status
+        )
+        
+            
