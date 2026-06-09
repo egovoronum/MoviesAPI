10a65805dbdd56d1e7c720ec73cb6f89f9d4a00b
Egor <egor.voronkov@gmail.com>
2026-06-09 08:42:44 +0300

    - added UserAPI class for user management

--- clients/auth_api.py
+++ clients/auth_api.py
@@ -4,7 +4,10 @@ from custom_requester.custom_requester import CustomRequester
 class AuthAPI(CustomRequester):
     
     def __init__(self, session):
-        super().__init__(session=session, base_url="https://auth.dev-cinescope.coconutqa.ru/")
+        super().__init__(
+            session=session, 
+            base_url="https://auth.dev-cinescope.coconutqa.ru/"
+        )
         
     
     def register_user(self, user_data, expected_status=201):
