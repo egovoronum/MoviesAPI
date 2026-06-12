a1926ef05b42d643236b627b5cd3794ef6fd9bb6
Egor Voronkov <egor.voronkov@gmail.com>
2026-06-12 11:41:38 +0300

    improved PEP compliance and removed unused variable in test_movies_negative

--- tests/negative/test_movies_negative.py
+++ tests/negative/test_movies_negative.py
@@ -2,14 +2,12 @@ from clients.api_manager import ApiManager
 
 #! minPrice > maxPrice in filter
 def test_invalid_price_filter(
-        unauthenticated_api_manager:ApiManager,
-        invalid_price_filter_reversed:dict
+        unauthenticated_api_manager: ApiManager,
+        invalid_price_filter_reversed: dict
     ):
 
-    params = invalid_price_filter_reversed
-
     response = unauthenticated_api_manager.movies_api.get_movies(
-        params, 
+        invalid_price_filter_reversed, 
         expected_status=400
     )
 
@@ -17,16 +15,15 @@ def test_invalid_price_filter(
     
     assert "minPrice must be less than maxPrice" in data["message"]
 
+
 #! negative price in filter
 def test_negative_price_filter(
-        unauthenticated_api_manager:ApiManager, 
-        invalid_price_filter_negative_min:dict
+        unauthenticated_api_manager: ApiManager, 
+        invalid_price_filter_negative_min: dict
     ):
 
-    params = invalid_price_filter_negative_min
-
     response = unauthenticated_api_manager.movies_api.get_movies(
-        params,
+        invalid_price_filter_negative_min,
         expected_status=400
     )
 
@@ -34,16 +31,15 @@ def test_negative_price_filter(
 
     assert "Поле minPrice имеет минимальную величину 0" in data["message"]
 
+
 #! negative page value in filter
 def test_negative_page_filter(
-        unauthenticated_api_manager:ApiManager,
-        invalid_page:dict
+        unauthenticated_api_manager: ApiManager,
+        invalid_page: dict
     ):
     
-    params = invalid_page
-
     response = unauthenticated_api_manager.movies_api.get_movies(
-        params,
+        invalid_page,
         expected_status=400
     )
 
@@ -53,16 +49,15 @@ def test_negative_page_filter(
         f"No 'message' field in 'response.json()'"
     )
 
+
 #! incorrect location value in filter
 def test_invalid_location_filter(
-        unauthenticated_api_manager:ApiManager,
-        invalid_location:dict       
+        unauthenticated_api_manager: ApiManager,
+        invalid_location: dict       
     ):
 
-    params = invalid_location
-
     response = unauthenticated_api_manager.movies_api.get_movies(
-        params,
+        invalid_location,
         expected_status=400
     )
 
@@ -75,13 +70,12 @@ def test_invalid_location_filter(
     "No 'message' field in 'response.json()'"
     )
 
+
 #! create invalid movie
 def test_create_invalid_movie(
-        admin_api_manager:ApiManager,
-        invalid_movie_data:dict
+        admin_api_manager: ApiManager,
+        invalid_movie_data: dict
     ):
-
-    movie_data = invalid_movie_data
 
     response = admin_api_manager.movies_api.create_movie(
         invalid_movie_data,
